06b0051cbd40e70b119652f62f69766a10aec80d
UnicornL-NgocLan <lengoclan8a22002@gmail.com>
2025-07-01 15:04:27 +0700

    Implement code changes to enhance functionality and improve performance

--- client/src/pages/Outlets/Tool.js
+++ client/src/pages/Outlets/Tool.js
@@ -1,5 +1,7 @@
 import MisaLogo from '../../images/logo-misa.png'
 import CalculatorImg from '../../images/calculator.png'
+import OpenProject from '../../images/open_project.png'
+import OneDrive from '../../images/one-drive.png'
 import { Button, Space } from 'antd'
 
 const Tools = () => {
@@ -10,8 +12,16 @@ const Tools = () => {
   const handleClickToBQCKCalculator = () => {
     window.open('/bqck-calculator', '_blank')
   }
+
+  const handleClickToQLCV = () => {
+    window.open('https://project.seacorp.vn/', '_blank')
+  }
+
+  const handleClickToSdrive = () => {
+    window.open('https://sdrive.seacorp.vn/', '_blank')
+  }
   return (
-    <Space>
+    <Space wrap={true}>
       <Button
         style={{ display: 'flex', alignItems: 'center' }}
         color="default"
@@ -29,6 +39,24 @@ const Tools = () => {
       >
         <span>Tính giá xuất kho BQCK</span>
         <img src={CalculatorImg} alt="" style={{ width: 15 }} />
+      </Button>
+      <Button
+        style={{ display: 'flex', alignItems: 'center' }}
+        color="default"
+        variant="outlined"
+        onClick={handleClickToSdrive}
+      >
+        <span>Quản lý tài liệu</span>
+        <img src={OneDrive} alt="" style={{ width: 17 }} />
+      </Button>
+      <Button
+        style={{ display: 'flex', alignItems: 'center' }}
+        color="default"
+        variant="outlined"
+        onClick={handleClickToQLCV}
+      >
+        <span>Quản lý công việc</span>
+        <img src={OpenProject} alt="" style={{ width: 27 }} />
       </Button>
     </Space>
   )
